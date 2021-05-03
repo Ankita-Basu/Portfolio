50d2e0acde06cb768961f11d8311240aad37d612
Ankita-Basu <60062665+Ankita-Basu@users.noreply.github.com>
2021-05-03 10:13:33 +0530

    new file structure, updated templates and static folder

--- pf/urls.py
+++ pf/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import HomePage, AboutPage, ContactPage
 
 urlpatterns = [
-    path('',HomePage.as_view(),name='home'),
+    path('home',HomePage.as_view(),name='home'),
     path('about',AboutPage.as_view(),name='about'),
     path('contact',ContactPage.as_view(),name='contact')
 ]
